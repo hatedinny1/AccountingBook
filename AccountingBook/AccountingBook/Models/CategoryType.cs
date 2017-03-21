75cb2a75d282f1f1d036d487ded7f4bf1c5a9271
Powen <hatedinny1@gmail.com>
2017-03-21 08:21:13 +0800

    1. [修改] 調整 css、js 載入為單一檔案 2. [新增] razor helper 顯示收入、支出顏色

---
+++ AccountingBook/AccountingBook/Models/CategoryType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AccountingBook.Models
+{
+    public class CategoryType
+    {
+        public enum Category
+        {
+            /// <summary>
+            /// 收入
+            /// </summary>
+            [Display(Name = "收入")]
+            Income = 0,
+            /// <summary>
+            /// 支出
+            /// </summary>
+            [Display(Name = "支出")]
+            Expenditure
+        }
+    }
+}
---
+++ AccountingBook/AccountingBook/Scripts/accountingBook.js
@@ -0,0 +1,7 @@
+﻿$('.remark-popover').webuiPopover('destroy').webuiPopover({
+    title: 'title',
+    animation: 'pop',
+    placement: 'bottom',
+    width: 300,
+    height: 200
+});

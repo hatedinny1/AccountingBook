06c16efbbca2d3c3ebca296d19432bc0082da8f2
Powen <hatedinny1@gmail.com>
2017-03-24 00:51:37 +0800

    1. AJAX 版本完成

---
+++ AccountingBook/AccountingBook/Controllers/AccountingBookV2Controller.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountingBook.Models;
+using AccountingBook.Models.Enum;
+using AccountingBook.Models.ViewModel;
+using AccountingBook.Service.Interface;
+using PagedList;
+
+namespace AccountingBook.Controllers
+{
+    public class AccountingBookV2Controller : Controller
+    {
+        private readonly IAccountBookService _accountBookSvc;
+
+        public AccountingBookV2Controller(IAccountBookService accountBookSvc)
+        {
+            _accountBookSvc = accountBookSvc;
+        }
+
+        // GET: AccountingBookV2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult AccountingDetail(int page = 1, int pageSize = 10)
+        {
+            var objectResult = _accountBookSvc
+                               .LookupAll()
+                               .Select(x => new AccountingBookViewModel
+                               {
+                                   Category = x.Categoryyy == 0 ? CategoryEnum.Expenditure : CategoryEnum.Income,
+                                   Date = x.Dateee,
+                                   Money = x.Amounttt,
+                                   Remark = x.Remarkkk
+                               })
+                               .OrderByDescending(x => x.Date)
+                               .ToPagedList(page, pageSize);
+            return PartialView(objectResult);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AccountingBookViewModel accountingBookViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = new AccountBook()
+                {
+                    Id = Guid.NewGuid(),
+                    Categoryyy = (int)accountingBookViewModel.Category,
+                    Amounttt = accountingBookViewModel.Money,
+                    Dateee = accountingBookViewModel.Date,
+                    Remarkkk = accountingBookViewModel.Remark
+                };
+                this._accountBookSvc.Create(model);
+                this._accountBookSvc.Commit();
+            }
+
+            var objectResult = _accountBookSvc
+                               .LookupAll()
+                               .Select(x => new AccountingBookViewModel
+                               {
+                                   Category = x.Categoryyy == 0 ? CategoryEnum.Expenditure : CategoryEnum.Income,
+                                   Date = x.Dateee,
+                                   Money = x.Amounttt,
+                                   Remark = x.Remarkkk
+                               })
+                               .OrderByDescending(x => x.Date)
+                               .ToPagedList(1, 10);
+
+            return PartialView("AccountingDetail", objectResult);
+        }
+    }
+}
